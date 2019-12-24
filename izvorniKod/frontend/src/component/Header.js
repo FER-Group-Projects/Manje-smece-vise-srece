896f9671c32b21f3237e954267b8dd1d0749584d
Marko Lazarić <marko.lazaric@fer.hr>
2019-12-24 21:15:07 +0100

    Separate header for logged in user

--- izvorniKod/frontend/src/component/Header.js
+++ izvorniKod/frontend/src/component/Header.js
@@ -1,27 +1,9 @@
 import React, { useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
 import { AuthStore } from '.././store/AuthStore'
-import { observer } from 'mobx-react'
 
 import NavDropdown from 'react-bootstrap/NavDropdown'
-import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
-
-const KonteinerButton = observer(() => {
-    if(AuthStore.getLoggedIn()!=='') return <button class='btn btn-lg btn-primary btn-block'>Kontejner</button>
-    return null
-})
-
-const LoginButton = observer(({ goToLogin }) => {
-    if(AuthStore.getLoggedIn()==='') return <button class='btn btn-lg btn-primary btn-block' onClick={goToLogin}>Prijava</button>
-    return null
-})
-
-const LogoutButton = observer(({ logout }) => {
-    if(AuthStore.getLoggedIn()!=='') return <button class='btn btn-lg btn-primary btn-block' onClick={logout}>Odjava</button>
-    return null
-})
-
 
 const Header = () => {
     let history = useHistory()
@@ -30,8 +12,16 @@ const Header = () => {
         history.push('/login')
     }
 
+    const goToSignUp = () => {
+        history.push('/signup')
+    }
+
     const goToIndex = () => {
         history.push('/')
+    }
+
+    const goToSettings = () => {
+        history.push('/settings')
     }
 
     const logout = () => {
@@ -47,16 +37,31 @@ const Header = () => {
         }
     },[])
 
+    let items
+
+    if (AuthStore.getLoggedIn() !== '') {
+        items = (
+            <NavDropdown alignRight={true} title={<i className="fas fa-user-circle fa-2x"></i>}>
+                <NavDropdown.Item onClick={goToSettings}>{AuthStore.getLoggedIn()}</NavDropdown.Item>
+                <NavDropdown.Divider/>
+                <NavDropdown.Item onClick={goToSettings}>Postavke</NavDropdown.Item>
+                <NavDropdown.Item onClick={logout}>Odjava</NavDropdown.Item>
+            </NavDropdown>
+        )
+    }
+    else {
+        items = (
+            <NavDropdown alignRight={true} title={<i className="fas fa-user-circle fa-2x"></i>}>
+                <NavDropdown.Item onClick={goToLogin}>Prijava</NavDropdown.Item>
+                <NavDropdown.Item onClick={goToSignUp}>Registracija</NavDropdown.Item>
+            </NavDropdown>
+        )
+    }
+
     return (
         <Navbar expand={false} bg="light" variant="light">
             <Navbar.Brand onClick={goToIndex}><i className="fas fa-dumpster"></i> Manje smeće više sreće</Navbar.Brand>
-            <NavDropdown alignRight={true} title={<i className="fas fa-user-circle fa-2x"></i>}>
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
+            {items}
         </Navbar>
     )
 }
