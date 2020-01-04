f1a42ea431b0f9e7f05bbd4eeffdcffa8aa7abfe
mlazaric <marko.lazaric@gmail.com>
2020-01-04 20:20:07 +0100

    Probaj novi dizajn

--- izvorniKod/frontend/src/component/Cards.js
+++ izvorniKod/frontend/src/component/Cards.js
@@ -13,9 +13,10 @@ const Cards = (props) => {
                 props.containers.map((value) => 
                     <div style={{
                         display:'flex', flexDirection:'column',
-                        borderRadius:'15px', backgroundColor:'#007bff',
-                        height:'150px', margin:'5px',width:'300px', 
-                        marginTop:'0px', justifyContent: 'center'
+                        borderRadius:'15px', backgroundColor:'#FFFFFF',
+                        height:'150px', margin:'10px',width:'300px',
+                        justifyContent: 'center',
+                        boxShadow: 'rgba(0, 0, 0, 0.2) 0px 1px 2px 0px, rgba(0, 0, 0, 0.19) 0px 1px 5px 0px'
                         }}>
                         <div style={
                             {display:'flex', flexDirection:'row'}}
@@ -31,7 +32,7 @@ const Cards = (props) => {
                             </div>
                         </div>
                         <div style={{display:'flex', flexDirection:'row', justifyContent:'space-evenly'}}>
-                            <MdEdit style={{backgroundColor:'white', borderRadius:'8px', height:'24px', width:'24px', borderRadius:'8px'}} 
+                            <MdEdit style={{backgroundColor:'white', borderRadius:'8px', height:'24px', width:'24px', borderRadius:'8px'}}
                                 onClick={() => history.push(`/kontejner/edit/${value.ID}`)}/>
                             <MdDelete style={{backgroundColor:'white', borderRadius:'8px', height:'24px', width:'24px', borderRadius:'8px'}}
                                 onClick={() => console.log('obrisi me!!')}/>
@@ -44,8 +45,9 @@ const Cards = (props) => {
                 props.zones.map((value) => 
                 <div style={{
                     display:'flex', flexDirection:'column',
-                    borderRadius:'15px', backgroundColor:'#007bff',
-                    margin:'5px', marginTop:'0px', width:'100px'
+                    borderRadius:'15px', backgroundColor:'#FFFFFF',
+                    margin:'10px', width:'150px',
+                    boxShadow: 'rgba(0, 0, 0, 0.2) 0px 1px 2px 0px, rgba(0, 0, 0, 0.19) 0px 1px 5px 0px'
                     }}>
                         <div style={{display:'flex', flexDirection:'column', margin:'10px', textAlign:'left'}}>
                             <span>ID: <span style={{textAlign:'right'}}>{value.ID}</span></span>
@@ -65,8 +67,9 @@ const Cards = (props) => {
                 props.workers.map((value) => 
                 <div style={{
                     display:'flex', flexDirection:'column',
-                    borderRadius:'15px', backgroundColor:'#007bff',
-                    margin:'5px', marginTop:'0px', width:'200px'
+                    borderRadius:'15px', backgroundColor:'#FFFFFF',
+                    margin:'10px', width:'200px',
+                    boxShadow: 'rgba(0, 0, 0, 0.2) 0px 1px 2px 0px, rgba(0, 0, 0, 0.19) 0px 1px 5px 0px'
                     }}>
                         <div style={{display:'flex', flexDirection:'column', margin:'10px', textAlign:'left'}}>
                             <span>Username: <span style={{textAlign:'right'}}>{value.Username}</span></span>
@@ -84,9 +87,10 @@ const Cards = (props) => {
                 props.containers &&
                 <div style={{
                     display:'flex', flexDirection:'column',
-                    borderRadius:'15px', backgroundColor:'#007bff',
-                    height:'150px', width:'300px', margin:'5px', 
-                    marginTop:'0px', justifyContent:'center', alignItems:'center'
+                    borderRadius:'15px', backgroundColor:'#FFFFFF',
+                    height:'150px', width:'300px', margin:'10px',
+                    justifyContent:'center', alignItems:'center',
+                    boxShadow: 'rgba(0, 0, 0, 0.2) 0px 1px 2px 0px, rgba(0, 0, 0, 0.19) 0px 1px 5px 0px'
                     }}>
                         <MdAddCircleOutline onClick={()=> history.push('kontejner/dodaj/')}
                             style={{height:'50px', width:'50px'}}/>
@@ -96,9 +100,10 @@ const Cards = (props) => {
                 props.zones &&
                 <div style={{
                     display:'flex', flexDirection:'column',
-                    borderRadius:'15px', backgroundColor:'#007bff',
-                    margin:'5px', marginTop:'0px', width: '100px',
-                    justifyContent:'center', alignItems:'center'
+                    borderRadius:'15px', backgroundColor:'#FFFFFF',
+                    margin:'10px', width: '100px',
+                    justifyContent:'center', alignItems:'center',
+                    boxShadow: 'rgba(0, 0, 0, 0.2) 0px 1px 2px 0px, rgba(0, 0, 0, 0.19) 0px 1px 5px 0px'
                     }}>
                     <MdAddCircleOutline onClick={()=> history.push('zone/dodaj')}
                         style={{height:'50px', width:'50px'}}/>
@@ -108,9 +113,10 @@ const Cards = (props) => {
                 props.workers &&
                 <div style={{
                     display:'flex', flexDirection:'column',
-                    borderRadius:'15px', backgroundColor:'#007bff',
-                    margin:'5px', marginTop:'0px',width: '200px',
-                    justifyContent:'center', alignItems:'center'
+                    borderRadius:'15px', backgroundColor:'#FFFFFF',
+                    margin:'10px', width: '200px',
+                    justifyContent:'center', alignItems:'center',
+                    boxShadow: 'rgba(0, 0, 0, 0.2) 0px 1px 2px 0px, rgba(0, 0, 0, 0.19) 0px 1px 5px 0px'
                     }}>
                     <MdAddCircleOutline onClick={()=> history.push('sluzbenici/dodaj')}
                         style={{height:'50px', width:'50px'}}/>
