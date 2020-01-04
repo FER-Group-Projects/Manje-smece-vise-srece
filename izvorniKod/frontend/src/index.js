ac44e33a3e0029969c967b6f4deac195008eaeed
mlazaric <marko.lazaric@gmail.com>
2020-01-04 20:36:43 +0100

    Popravi autentikaciju

--- izvorniKod/frontend/src/index.js
+++ izvorniKod/frontend/src/index.js
@@ -5,8 +5,19 @@ import Loginform from './forms/login-form'
 import App from './routes/router'
 import axios from 'axios'
 import 'bootstrap/dist/css/bootstrap.css'
+import {AuthStore} from "./store/AuthStore";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+axios.interceptors.request.use(function (config) {
+    const token = localStorage.getItem('token') || '';
+
+    if (token !== undefined) {
+        config.headers.Authorization = token;
+        AuthStore.setToken(token)
+    }
+
+    return config;
+});
 
 serviceWorker.unregister();
