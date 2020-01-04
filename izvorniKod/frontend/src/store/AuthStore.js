a0c18fffbcd6f83b9bfcdad21fed7053d6c76c0d
mlazaric <marko.lazaric@gmail.com>
2020-01-04 19:30:06 +0100

    Dodaj token autorizaciju i dohvat uloga

--- izvorniKod/frontend/src/store/AuthStore.js
+++ izvorniKod/frontend/src/store/AuthStore.js
@@ -1,7 +1,27 @@
 import { observable } from 'mobx'
+import axios from 'axios'
 
 class store {
     loggedIn = observable({loggedIn: ''})
+    token = ''
+    roles = []
+
+    setToken(token) {
+        this.token = token
+        axios.defaults.headers.common['Authorization'] = token
+    }
+
+    getToken() {
+        return this.token
+    }
+
+    setRoles(roles) {
+        this.roles = roles
+    }
+
+    getRoles() {
+        return this.roles
+    }
 
     setLoggedIn(username) {
         localStorage.setItem('username', username)
