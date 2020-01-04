a9153233c82d698e88f1fd9f5fdde6ff83130778
mlazaric <marko.lazaric@gmail.com>
2020-01-04 19:50:44 +0100

    Poveži kontejnere i zone s backendom

--- izvorniKod/frontend/src/component/MainWindow.js
+++ izvorniKod/frontend/src/component/MainWindow.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react'
 import GarbageMap from './MapComponent'
+import axios from "axios";
 
-const MainWindow = () => {
+class MainWindow extends Component {
+  constructor() {
+    super()
+    this.state = { containers: [] }
+  }
 
-  return (
-      <GarbageMap/>
-  )
+  componentDidMount() {
+    axios.get('/waste-containers/all')
+        .then((e) => {
+          this.setState({containers: e.data.map((container) => ({lat: container.latitude, lng: container.longitude}))})
+        }).catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+        <GarbageMap containers={this.state.containers}/>
+    )
+  }
 }
 
 export default MainWindow
--- izvorniKod/frontend/src/component/Zones/Zones.js
+++ izvorniKod/frontend/src/component/Zones/Zones.js
@@ -1,18 +1,25 @@
 import React from 'react'
 import Cards from '../Cards'
+import axios from "axios";
 
-const Zones = () => {
+class Zones extends React.Component{
+    constructor() {
+        super()
+        this.state = { zones: [] }
+    }
 
-    return(
-        <Cards zones={[
-            {ID:1234, Ime: 'Z1', },
-            {ID:1234, Ime: 'Z1', },
-            {ID:1234, Ime: 'Z1', },
-            {ID:1234, Ime: 'Z1', },
-            {ID:1234, Ime: 'Z1', },
-            {ID:1234, Ime: 'Z1', },
-        ]}/>
-    )
+    componentDidMount() {
+        axios.get('/zones/all')
+            .then((e) => {
+                this.setState({zones: e.data.map((zone) => ({ID: zone.id, Ime: zone.zoneName}))})
+            }).catch(err => console.log(err));
+    }
+
+    render() {
+        return(
+            <Cards zones={this.state.zones}/>
+        )
+    }
 }
 
 export default Zones
