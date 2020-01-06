d2876723106f101d3ccc98b3b87101c893b60e2e
Marko Lazarić <marko.lazaric@fer.hr>
2020-01-06 14:55:10 +0100

    Pretraživanje kontejnera

--- izvorniKod/frontend/src/component/ContainerSearch.js
+++ izvorniKod/frontend/src/component/ContainerSearch.js
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 import GarbageMap from './MapComponent'
 import { FaSearch } from 'react-icons/fa'
+import {useHistory} from "react-router-dom";
 
 const ContainerSearch = () => {
-    return(
+    const [ID, setID] = useState("")
+    let history = useHistory()
+
+    async function IDChanged(event) {
+        setID(event.target.value)
+    }
+
+    function gotoContainer() {
+        if (ID !== "") {
+            history.push('/kontejner/' + ID)
+        }
+    }
+
+    return (
         <div style={{display: 'contents'}}>
             <div class="input-group flex-nowrap"
                 style={{zIndex: 2, marginLeft: '100px', width:'150px', position: 'absolute'}}>
-                <input type="text" class="form-control" placeholder="ID" aria-label="Username" aria-describedby="addon-wrapping"/>
-                <div onClick={() => {console.log('hello')}} class="input-group-append">
+                <input type="text" class="form-control" placeholder="ID" aria-label="Username" aria-describedby="addon-wrapping" onChange={IDChanged}/>
+                <div onClick={gotoContainer} class="input-group-append">
                     <span class="input-group-text" id="addon-wrapping">
                         <FaSearch/>
                     </span>
